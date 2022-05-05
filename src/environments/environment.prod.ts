78639f4f7433cfa8cb79607e9ccdbe37a5658965
Albert Xifra <axifra.dam@institutcampalans.net>
2022-05-05 16:54:59 +0200

    firebase installed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'bddpictogrames',
+    appId: '1:900088564570:web:046f8346598d443c48c163',
+    storageBucket: 'bddpictogrames.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBOHJCBPGF-zRo0rDBQutj9WNO2CYE_49M',
+    authDomain: 'bddpictogrames.firebaseapp.com',
+    messagingSenderId: '900088564570',
+  },
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBOHJCBPGF-zRo0rDBQutj9WNO2CYE_49M",
+    authDomain: "bddpictogrames.firebaseapp.com",
+    projectId: "bddpictogrames",
+    storageBucket: "bddpictogrames.appspot.com",
+    messagingSenderId: "900088564570",
+    appId: "1:900088564570:web:046f8346598d443c48c163"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'bddpictogrames',
+    appId: '1:900088564570:web:046f8346598d443c48c163',
+    storageBucket: 'bddpictogrames.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBOHJCBPGF-zRo0rDBQutj9WNO2CYE_49M',
+    authDomain: 'bddpictogrames.firebaseapp.com',
+    messagingSenderId: '900088564570',
+  },
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBOHJCBPGF-zRo0rDBQutj9WNO2CYE_49M",
+    authDomain: "bddpictogrames.firebaseapp.com",
+    projectId: "bddpictogrames",
+    storageBucket: "bddpictogrames.appspot.com",
+    messagingSenderId: "900088564570",
+    appId: "1:900088564570:web:046f8346598d443c48c163"
+  }
 };
 
 /*
